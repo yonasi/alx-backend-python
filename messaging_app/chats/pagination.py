862a416fbb717edfdeb1eea661a2cc65a535e8b2
Yonas Mamo <Yonasma416@gmail.com>
2025-07-25 17:19:49 +0300

    bug fix on pagination feature3

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,9 +1,18 @@
 from rest_framework.pagination import PageNumberPagination
 
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
 class MessagePagination(PageNumberPagination):
-    """
-    Custom pagination class for messages, setting a default page size of 20.
-    """
-    page_size = 20 # Number of messages per page
-    page_size_query_param = 'page_size' # Allows client to specify page size (e.g., ?page_size=10)
-    max_page_size = 100 # Maximum page size allowed if client requests more
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'num_pages': self.page.paginator.num_pages,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -92,18 +92,3 @@ class MessageViewSet(viewsets.ModelViewSet):
             raise ForbiddenException({"detail": "You are not a participant of this conversation."})
         serializer.save(sender=self.request.user, conversation=conversation)
 
-
-
-class MessagePagination(PageNumberPagination):
-    page_size = 20
-    page_size_query_param = 'page_size'
-    max_page_size = 100
-
-    def get_paginated_response(self, data):
-        return Response({
-            'count': self.page.paginator.count,
-            'num_pages': self.page.paginator.num_pages,
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
-            'results': data
-        })
