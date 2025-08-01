22463d870768d434b1181af493cec6e1c9ad55fc
Yonas Mamo <Yonasma416@gmail.com>
2025-08-01 17:50:23 +0300

    directory restructured for 3rd time

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -84,7 +84,7 @@ class OffensiveLanguageMiddleware:
 
 
 
-class RolePermissionMiddleware:
+class RolepermissionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
